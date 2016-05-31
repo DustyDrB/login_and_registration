8eeb77d87530ac3aff2329e43244aa9adbab3c8e
Dustin Brown <Dustin Brown>
2016-05-31 17:28:39 -0400

    update server and index

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import re
 from flask.ext.bcrypt import Bcrypt
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 mysql = MySQLConnector(app, 'loginreg')
 app.secret_key = "adorable_beagles"
 
@@ -11,17 +12,36 @@ mail_val = re.compile(r'^[a-za-z0-9\.\+_-]+@[a-za-z0-9\._-]+\.[a-za-z]*$')
 num_check = re.compile('[0-9]')
 case_check = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])')
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
 	return render_template('index.html')
 
 @app.route('/login', methods=['POST'])
 def login():
+	email = request.form['email']
+	password = request.form['password']
+	query = "SELECT * FROM users WHERE email= :email LIMIT 1"
+	data = {
+		'email': request.form['logemail'],
+		'password': request.form['logpass']
+	}
+	mysql.query_db(query, data)
+	user = mysql.query_db(query, data)
+	email = mysql.query_db(query, data)[0]
+	if bcrypt.check_password_hash(user[0]['pw_hash'], password):
+		return render_template('/', email=email)
+	else:
+		flash('Incorrect password')
+		return redirect('/')
 
-	return redirect('/')
-
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['POST', 'GET'])
 def add_user():
+	session['first_name'] = request.form['first_name']
+	session['last_name'] = request.form['last_name']
+	session['email'] = request.form['email']
+	session['pass'] = request.form['pass']
+	session['confirm_pass'] = request.form['confirm_pass']
+
 	if len(request.form['first_name']) < 1:
 		flash('First Name cannot be empty!')
 		return redirect('/')
@@ -53,16 +73,19 @@ def add_user():
 		return redirect('/')
   	else:
   		flash("Registration successful! Thank you!")
-
-  		query = "INSERT INTO users (first_name, last_name, email, password, updated_at, created_at) VALUES (:first_name, :last_name, :email, :pass, NOW(), NOW())"
-		pw_hash = bcrypt.generate_password_hash(reqest.form['password'])
+  		first_name = request.form['first_name']
+		last_name = request.form['last_name']
+		email = request.form['email']
+		password = request.form['password']
+  		user_query = "INSERT INTO users (first_name, last_name, email, password, updated_at, created_at) VALUES (:first_name, :last_name, :email, :pass, NOW(), NOW())"
 		data = {
-			first_name: request.form['first_name'],
-			last_name: request.form['last_name'],
-			email: request.form['email'],
-			password: request.form['password'],
+			'first_name': request.form['first_name'],
+			'last_name': request.form['last_name'],
+			'email': request.form['email'],
+			'pw': request.form['password'],
 		}
-		mysql.query_db(query, data)
+		pw_hash = bcrypt.generate_password_hash(password)
+		mysql.query_db(user_query, data)
 		return redirect('/')
 
 	return redirect('/')
